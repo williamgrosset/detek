c7bfffbad23d4e86e03458e7059ad93b8eabb408
William Grosset <williamhgrosset@gmail.com>
2018-02-22 00:24:57 -0800

    Parse and print each packet found in capture file

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import pcapy
 def main():
     print('Hello, world')
     pc = pcapy.open_offline(sys.argv[1])
+    data = pc.next()
+    while data[0]:
+        print(data)
+        data = pc.next()
 
 if __name__ == '__main__':
     main()
