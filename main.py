6525a0c6bf7457a7befd62e20b0fa229b78e85ec
William Grosset <williamhgrosset@gmail.com>
2018-02-22 22:47:36 -0800

    Identify connection within each packet

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import sys
 import pcapy
-import impacket
+from impacket import ImpactDecoder
+
+def callback(header, data):
+    decoder = ImpactDecoder.EthDecoder()
+    ethernet_packet = decoder.decode(data)
+    ip_header = ethernet_packet.child()
+    source_ip = ip_header.get_ip_src()
+    dest_ip = ip_header.get_ip_dst()
+    print "Connection: %s -> %s" % (source_ip, dest_ip)
+
 
 def main():
     pc = pcapy.open_offline(sys.argv[1])
     pc.setfilter('tcp')
 
-    data = pc.next()
-    total_packets = 0
-    while data[0]:
-        # print(data)
-        data = pc.next()
-        total_packets += 1
-
-    print('Total number of packets:', total_packets)
+    pc.loop(0, callback)
 
 if __name__ == '__main__':
     main()
